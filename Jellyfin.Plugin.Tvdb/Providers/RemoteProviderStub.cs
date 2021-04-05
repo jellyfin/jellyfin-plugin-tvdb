74bb1ab8adf97c40eaae13127ed476618fd533e8
cvium <clausvium@gmail.com>
2021-04-05 23:07:54 +0200

    Add missing episode provider
    And minor fixes to caching

---
+++ Jellyfin.Plugin.Tvdb/Providers/RemoteProviderStub.cs
@@ -0,0 +1,14 @@
+﻿using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+
+namespace Jellyfin.Plugin.Tvdb.Providers
+{
+    /// <summary>
+    /// The metadata provider class for allowing Library-based configuration.
+    /// </summary>
+    public class RemoteProviderStub : IMetadataProvider<Series>, IRemoteMetadataProvider
+    {
+        /// <inheritdoc />
+        public string Name => TvdbMissingEpisodeProvider.ProviderName;
+    }
+}
