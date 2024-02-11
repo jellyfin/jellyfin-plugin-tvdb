e8889471ab3e89bc02543533f300b5f3d5dfd53e
Cody Robibero <cody@robibe.ro>
2024-02-11 12:33:11 -0700

    Add the project's api key

--- Jellyfin.Plugin.Tvdb/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Tvdb/Configuration/PluginConfiguration.cs
@@ -10,7 +10,7 @@ namespace Jellyfin.Plugin.Tvdb.Configuration
         /// <summary>
         /// Gets the tvdb api key for project.
         /// </summary>
-        public const string ProjectApiKey = "";
+        public const string ProjectApiKey = "7f7eed88-2530-4f84-8ee7-f154471b8f87";
 
         /// <summary>
         /// Gets or sets the tvdb api key for user.
