9b7b3507809c387beb7035a88f44029b37832251
LJQ <leejunquan7@gmail.com>
2024-07-16 00:49:42 +0800

    Add Metadata fetcher tvdb movie

--- Jellyfin.Plugin.Tvdb/Providers/TvdbMovieProvider.cs
+++ Jellyfin.Plugin.Tvdb/Providers/TvdbMovieProvider.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Jellyfin.Data.Enums;
 using Jellyfin.Extensions;
 using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Providers;
@@ -63,9 +66,31 @@ namespace Jellyfin.Plugin.Tvdb.Providers
         }
 
         /// <inheritdoc/>
-        public Task<MetadataResult<Movie>> GetMetadata(MovieInfo info, CancellationToken cancellationToken)
+        public async Task<MetadataResult<Movie>> GetMetadata(MovieInfo info, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var result = new MetadataResult<Movie>
+            {
+                QueriedById = true,
+            };
+
+            if (!info.IsSupported())
+            {
+                result.QueriedById = false;
+                await Identify(info).ConfigureAwait(false);
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (info.IsSupported())
+            {
+                result.Item = new Movie();
+                result.HasMetadata = true;
+
+                await FetchMovieMetadata(result, info, cancellationToken)
+                    .ConfigureAwait(false);
+            }
+
+            return result;
         }
 
         private async Task<IEnumerable<RemoteSearchResult>> FetchMovieSearchResult(MovieInfo movieInfo, CancellationToken cancellationToken)
@@ -265,6 +290,176 @@ namespace Jellyfin.Plugin.Tvdb.Providers
                 .Select(i => i.Item2)
                 .Take(MaxSearchResults) // TVDB returns a lot of unrelated results
                 .ToList();
+        }
+
+        private async Task Identify(MovieInfo info)
+        {
+            if (info.HasTvdbId())
+            {
+                return;
+            }
+
+            var remoteSearchResults = await FindMovie(info.Name, info.Year, info.MetadataLanguage, CancellationToken.None)
+                .ConfigureAwait(false);
+
+            var entry = remoteSearchResults.FirstOrDefault();
+            if (entry.HasTvdbId(out var tvdbId))
+            {
+                info.SetTvdbId(tvdbId);
+            }
+        }
+
+        private async Task FetchMovieMetadata(
+            MetadataResult<Movie> result,
+            MovieInfo movieInfo,
+            CancellationToken cancellationToken)
+        {
+            var movieMetadata = result.Item;
+            async Task<string?> TryGetTvdbIdWithRemoteId(string id)
+            {
+                return await GetMovieByRemoteId(
+                    id,
+                    cancellationToken).ConfigureAwait(false);
+            }
+
+            if (movieInfo.HasTvdbId(out var tvdbIdTxt))
+            {
+                movieMetadata.SetTvdbId(tvdbIdTxt);
+            }
+
+            if (movieInfo.HasProviderId(MetadataProvider.Imdb, out var imdbId))
+            {
+                movieMetadata.SetProviderId(MetadataProvider.Imdb, imdbId!);
+                tvdbIdTxt ??= await TryGetTvdbIdWithRemoteId(imdbId!).ConfigureAwait(false);
+            }
+
+            if (movieInfo.HasProviderId(MetadataProvider.Zap2It, out var zap2It))
+            {
+                movieMetadata.SetProviderId(MetadataProvider.Zap2It, zap2It!);
+                tvdbIdTxt ??= await TryGetTvdbIdWithRemoteId(zap2It!).ConfigureAwait(false);
+            }
+
+            if (movieInfo.HasProviderId(MetadataProvider.Tmdb, out var tmdbId))
+            {
+                movieMetadata.SetProviderId(MetadataProvider.Tmdb, tmdbId!);
+                tvdbIdTxt ??= await TryGetTvdbIdWithRemoteId(tmdbId!).ConfigureAwait(false);
+            }
+
+            if (string.IsNullOrWhiteSpace(tvdbIdTxt))
+            {
+                _logger.LogWarning("No valid tvdb id found for movie {TvdbId}:{MovieName}", tvdbIdTxt, movieInfo.Name);
+                return;
+            }
+
+            var tvdbId = Convert.ToInt32(tvdbIdTxt, CultureInfo.InvariantCulture);
+            try
+            {
+                var movieResult =
+                    await _tvdbClientManager
+                        .GetMovieExtendedByIdAsync(tvdbId, cancellationToken)
+                        .ConfigureAwait(false);
+                MapMovieToResult(result, movieResult, movieInfo);
+
+                result.ResetPeople();
+
+                List<Character> people = new List<Character>();
+                if (movieResult.Characters is not null)
+                {
+                    foreach (Character character in movieResult.Characters)
+                    {
+                        people.Add(character);
+                    }
+
+                    MapActorsToResult(result, people);
+                }
+                else
+                {
+                    _logger.LogError("Failed to retrieve actors for movie {TvdbId}:{MovieName}", tvdbId, movieInfo.Name);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to retrieve movie with id {TvdbId}:{MovieName}", tvdbId, movieInfo.Name);
+                return;
+            }
+        }
+
+        private void MapMovieToResult(MetadataResult<Movie> result, MovieExtendedRecord tvdbMovie, MovieInfo info)
+        {
+            Movie movie = result.Item;
+            movie.SetTvdbId(tvdbMovie.Id);
+            // Tvdb uses 3 letter code for language (prob ISO 639-2)
+            // Reverts to OriginalName if no translation is found
+            movie.Name = tvdbMovie.Translations.GetTranslatedNamedOrDefault(info.MetadataLanguage) ?? TvdbUtils.ReturnOriginalLanguageOrDefault(tvdbMovie.Name);
+            movie.Overview = tvdbMovie.Translations.GetTranslatedOverviewOrDefault(info.MetadataLanguage);
+            movie.OriginalTitle = tvdbMovie.Name;
+            result.ResultLanguage = info.MetadataLanguage;
+            // Attempts to default to USA if not found
+            movie.OfficialRating = tvdbMovie.ContentRatings.FirstOrDefault(x => string.Equals(x.Country, TvdbCultureInfo.GetCountryInfo(info.MetadataCountryCode)?.ThreeLetterISORegionName, StringComparison.OrdinalIgnoreCase))?.Name ?? tvdbMovie.ContentRatings.FirstOrDefault(x => string.Equals(x.Country, "usa", StringComparison.OrdinalIgnoreCase))?.Name;
+
+            var collectionIds = tvdbMovie.Lists.Where(x => x.IsOfficial is true)
+                .Select(x => x.Id?.ToString(CultureInfo.InvariantCulture))
+                .Aggregate(new StringBuilder(), (sb, id) => sb.Append(id).Append(';'));
+            movie.SetProviderIdIfHasValue(TvdbPlugin.CollectionProviderId, collectionIds.ToString());
+
+            movie.SetProviderIdIfHasValue(TvdbPlugin.SlugProviderId, tvdbMovie.Slug);
+
+            var imdbId = tvdbMovie.RemoteIds?.FirstOrDefault(x => string.Equals(x.SourceName, "IMDB", StringComparison.OrdinalIgnoreCase))?.Id.ToString();
+            movie.SetProviderIdIfHasValue(MetadataProvider.Imdb, imdbId);
+
+            var zap2ItId = tvdbMovie.RemoteIds?.FirstOrDefault(x => string.Equals(x.SourceName, "Zap2It", StringComparison.OrdinalIgnoreCase))?.Id.ToString();
+            movie.SetProviderIdIfHasValue(MetadataProvider.Zap2It, zap2ItId);
+
+            var tmdbId = tvdbMovie.RemoteIds?.FirstOrDefault(x => string.Equals(x.SourceName, "TheMovieDB.com", StringComparison.OrdinalIgnoreCase))?.Id.ToString();
+            movie.SetProviderIdIfHasValue(MetadataProvider.Tmdb, tmdbId);
+
+            if (DateTime.TryParse(tvdbMovie.First_release.Date, out var date))
+            {
+                // dates from tvdb are UTC but without offset or Z
+                movie.PremiereDate = date;
+                movie.ProductionYear = date.Year;
+            }
+
+            if (tvdbMovie.Runtime is not null)
+            {
+                movie.RunTimeTicks = TimeSpan.FromMinutes(tvdbMovie.Runtime.Value).Ticks;
+            }
+
+            foreach (var genre in tvdbMovie.Genres)
+            {
+                movie.AddGenre(genre.Name);
+            }
+
+            if (tvdbMovie.Companies.Studio is not null)
+            {
+                foreach (var studio in tvdbMovie.Companies.Studio)
+                {
+                    movie.AddStudio(studio.Name);
+                }
+            }
+        }
+
+        private static void MapActorsToResult(MetadataResult<Movie> result, IEnumerable<Character> actors)
+        {
+            foreach (Character actor in actors)
+            {
+                var personInfo = new PersonInfo
+                {
+                    Type = PersonKind.Actor,
+                    Name = (actor.PersonName ?? string.Empty).Trim(),
+                    Role = actor.Name
+                };
+
+                if (!string.IsNullOrEmpty(actor.PersonImgURL))
+                {
+                    personInfo.ImageUrl = actor.PersonImgURL;
+                }
+
+                if (!string.IsNullOrWhiteSpace(personInfo.Name))
+                {
+                    result.AddPerson(personInfo);
+                }
+            }
         }
 
         /// <inheritdoc/>
